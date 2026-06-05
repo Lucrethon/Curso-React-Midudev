e43a493dea2dbe437dc0f75ffe825a07143647bb
Lucrethon <lucrecimich@gmail.com>
2026-06-05 13:33:59 -0400

    JSX introduction

--- Level_1/script.js
+++ Level_1/script.js
@@ -1,3 +1,4 @@
+// -------------- Import Reac ------------------
 
 // Importas ReactDOM desde la URL de la CDN
 import ReactDOM from "https://esm.sh/react-dom@18.2.0/client";
@@ -5,6 +6,7 @@ import ReactDOM from "https://esm.sh/react-dom@18.2.0/client";
 // Nota: Para que React funcione, usualmente también necesitarás importar React:
 import React from "https://esm.sh/react@18.2.0";
 
+// Se puede aprender react sin instalar nada
 
 //Olvídate de npm install: Al usar esm.sh, estás cargando los módulos directamente desde la web (CDN). Es una excelente opción para prototipos rápidos o proyectos pequeños sin configuraciones complejas de Webpack o Vite.
 
@@ -65,6 +67,82 @@ button.addEventListener("click", function() {
 // Describimos lo que quiero que haga
 
 // ------------------- React ----------------------
+
+// Tenemos que tener un sitio donde renderizar nuestra APP, asi que se empieza por un div en HTML
+
+const appDomElement = document.getElementById("root");
+
+// React se puede utilizar para inyectarlo en un componente en específico en cualquier web
+// En React se crea como un "arbol de comopentes". Y se empieza desde una base que se crea así: 
+
+const root = ReactDOM.createRoot(appDomElement);
+// Una vez creado, ya podemos decirle que queremos renderizar dentro
+
+root.render("Hola React")
+// Dentro de "render", no se pueden renderizar texto HTML plano: 
+
+// root.render("<button>Me gusta>button>") <- ESTO NO SE PUEDE
+
+// Esto es por un tema de seguridad: asi no se le puede inyectar codigo a react
+
+// Se tiene que crear el elemento HTML para decirle a React que queremos renderizar concretamente 
+
+const buttonReact1 = React.createElement("button", {"dataid": 123}, "Button 1"); 
+const buttonReact2 = React.createElement("button", {"dataid": 456}, "Button 2"); 
+const buttonReact3 = React.createElement("button", {"dataid": 789}, "Button 3"); 
+
+
+// Parametro de React.createElement()
+
+// Parametro 1: El elemento a crear
+// Parametro 2: los atributos o propiedades que le queremos dar (Class name, etc). Esto es un objeto
+// Parametro 3: texto (innerHTML)
+
+// Para renderizar los tres botones, no podemos hacer: 
+
+// root.render(buttonReact1, buttonReact2, buttonReact3); <= ESTO NO ES CORRECTO
+
+// No se pueden renderizar mas de un elemento a la vez
+
+// Lo que se hace es crear un elemento que envuelva a todos los elementos que queremos renderizar (como un div que contiene los buttons):
+
+const div = React.createElement("div", null, [buttonReact1, buttonReact2, buttonReact3]); 
+root.render(div); 
+// Aqui ya se renderizan los tres botones  
+
+// Pero que pasa si no quieres utilizar un Div? 
+// React tiene un componente propio para envolver elementos: React.Fragment 
+
+const fragment = React.createElement(React.Fragment, null, [buttonReact1, buttonReact2, buttonReact3]); 
+root.render(fragment); 
+// Aqui ya se renderizan los tres botones, pero se estan rendetizando encima de los anteriores 
+
+// Es mejor un React.Fragment para dejar el HTML lo más limpio posible 
+
+// El codigo hasta aquí TODAVIA NO ES DECLARATIVO 
+// Para esto, hay que utilizar JSX: una extención de JS para describir la interfaz de usuario que quieres. Algo así: 
+
+
+// ------------------ JSX ------------------------
+
+// Esto es el mismo codigo que hicimos arriba 
+
+<React.Fragment>
+    <button data-id="123">Button 1</button>
+    <button data-id="456">Button 2</button>
+    <button data-id="789">Button 3</button>
+</React.Fragment>
+
+// Quien se encarga de transformarlo? 
+// Babel, SWC
+// Son transpiladores o compiladores de codigo donde nosotros escribimos el codigo JSX y lo tranforma a la versión larga de antes 
+
+// JSX NO es codigo HTML, es codigo JS 
+
+// Cosas a saber de JSX: 
+
+// 1. Se pueden colocar expresiones entre corchetes (constantes, funciones, etc. Cosas que devuelvan un valor): 
+//    <h1>Hola {1 + 1}</h1> 
 
 
 
