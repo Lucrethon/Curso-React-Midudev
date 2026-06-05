b9d354d37051236084475881263d221ca50a02e0
Lucrethon <lucrecimich@gmail.com>
2026-06-05 11:13:42 -0400

    import react

---
+++ Level_1/script.js
@@ -0,0 +1,10 @@
+
+// Importas ReactDOM desde la URL de la CDN
+import ReactDOM from "https://esm.sh/react-dom@18.2.0/client";
+
+// Nota: Para que React funcione, usualmente también necesitarás importar React:
+import React from "https://esm.sh/react@18.2.0";
+
+
+
+//Olvídate de npm install: Al usar esm.sh, estás cargando los módulos directamente desde la web (CDN). Es una excelente opción para prototipos rápidos o proyectos pequeños sin configuraciones complejas de Webpack o Vite.
