a59c367e90b33e1d05d3c46317056c20d784f3e5
Lucrethon <lucrecimich@gmail.com>
2026-06-12 18:42:41 -0400

    Refactor comments and improve code clarity in App.tsx

--- Projects/03_La_Vieja_Game/src/App.tsx
+++ Projects/03_La_Vieja_Game/src/App.tsx
@@ -14,7 +14,6 @@ const WINNER = {
   isTie: false,
 } as const;
 
-// cuando no se le agrega el "as const" a los objetos ENUM, TypeScript interpreta sus propiedades como string genéricos en lugar de valores literales exactos como "tie".
 
 const WINNER_COMBOS = [
     [0, 1, 2],
@@ -137,8 +136,8 @@ const checkWinner = (boardToCheck: (null | string)[]): WinnerState => {
       // por eso hay que decirle explicitamente a la funcion que no nos va a retornar un string normal, nos va a retornar el type WinnerState
     }
 }
-  // si hay empate:
-  if (!boardToCheck.includes(null)) {return WINNER.isTie as WinnerState}
+  // cuando todas las posiciones son diferentes a null y ninguna de las combinacniones anteriores se dio, es un empate:
+  if (!boardToCheck.includes(null)) {return WINNER.isTie as WinnerState} // => false 
 
 
     // Si no hay ganador ni empate, y el juego sigue en curso:
@@ -233,16 +232,18 @@ function resetGame () {
 
 function displayBoard(boardToCheck: (null | string)[]) {
 
-    return boardToCheck.map((___, index) => {
+    return boardToCheck.map((square, index) => {
       return (
         <Square 
         key={index}
         updateBoard={() => updateBoard(boardToCheck, index)}
         >
-          {board[index]}
+          {square}
         </Square>
       )
     })
+// square representa el valor actual de esa posición en el tablero (por ejemplo: "X", "O", o null si está vacío
+
 // aqui no podemos pasar la ejecución de la función (updateBoard()) porque se estaria ejecutandp cada vex que se rendericen los elementos de map
 // Queremos ejecutarla cuando el usuario haga click
 // para eso, tenemos que llamar a la función updateBoard con una función flecha anonima
@@ -304,3 +305,28 @@ function displayBoard(boardToCheck: (null | string)[]) {
 }
 
 export default App
+
+// operador && logico: evalúa los valores de izquierda a derecha
+
+// Si el primer valor es falso (false, null, undefined, 0, ""), JavaScript se detiene inmediatamente y devuelve ese valor falso. No sigue leyendo el resto.
+// Si el primer valor es verdadero (truthy), JavaScript continúa y devuelve el segundo valor.
+
+// React está diseñado para ignorar y no pintar nada en la pantalla si un elemento devuelve false, null o undefined.
+
+// ------------------ renderizado condicional ------------------------
+
+// Usa && cuando solo te interesa que se muestre algo si la condición es verdadera, y si es falsa no quieres hacer absolutamente nada.
+// Usa el ternario (? :) cuando tienes un plan A (si es verdadero) y un plan B (si es falso), como en tu código, donde si es false muestra "Empate" y si no, muestra "El ganador es:".
+
+
+// ------------------------ ventada modal ---------------------------
+
+// crear una ventana modal en JSX : 
+
+// 1. Abres llaves {} en tu JSX.
+
+// 2. Usas el operador && con tu condición .
+
+// 3. Abres parentesis y metes el JSX (la estructura de tu modal).
+
+// 4. Le aplicas CSS para que flote sobre la pantalla.
