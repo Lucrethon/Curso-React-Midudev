8b89da4f101676c13dca556a3a938803eab6328d
Lucrethon <lucrecimich@gmail.com>
2026-06-12 16:22:55 -0400

    Refactor game logic and state management for Tic Tac Toe

--- Projects/03_La_Vieja_Game/src/App.tsx
+++ Projects/03_La_Vieja_Game/src/App.tsx
@@ -1,17 +1,20 @@
-import { use, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 import './index.css'
 import React from 'react'
 
 
 const TURNS = {
-  x: "x",
-  o: "o",
-};
+  X: "x",
+  O: "o",
+} as const;
+
 
 const WINNER = {
-  tie: "nobody won",
-};
+  isTie: false,
+} as const;
+
+// cuando no se le agrega el "as const" a los objetos ENUM, TypeScript interpreta sus propiedades como string genéricos en lugar de valores literales exactos como "tie".
 
 const WINNER_COMBOS = [
     [0, 1, 2],
@@ -23,18 +26,23 @@ const WINNER_COMBOS = [
     [0, 4, 8],
     [2, 4, 6],
 
-]
+]; 
+
+type WinnerState = 'x' | 'o' | false | null
+type TurnState = 'x' | 'o'
+
 
 
 // -------------------- Componente Square: posiciones en el tablero ----------------
 
 const Square = (
   { children, isSelected = false, updateBoard }: 
+
   { children: React.ReactNode, 
     isSelected?: boolean, 
     updateBoard?: () => void }) => {
 
-  const className = `square ${(isSelected ? "is-selected" : "")}`;
+  const className = `square ${(isSelected ? "is-selected" : "")}`.trim();
 
   const handleClick = () => {
     if (updateBoard) {
@@ -81,57 +89,82 @@ const Square = (
 
 function App() {
 
+  // ------------------------------- ESTADOS ---------------------------------------
+
   // ---------------- estado tablero: posiciones con un array de 9 ---------------------
 
 const [board, setBoard] = useState(Array(9).fill(null)); // los valores iniciales son null
 
 // ---------------- estado: turnos ----------------------
 
-const [turn, setTurn] = useState(TURNS.x); // el turno inicial es de x
+const [turn, setTurn] = useState<TurnState>(TURNS.X); // el estado inicial es x (turno de x)
 
-// --------------- Estado: ganador -----------------
+// --------------- Estado: ganador ----------------- 
 
-const [winner, setWinner] = useState(WINNER.tie)
+const [winner, setWinner ] = useState<WinnerState>(null) // el estado inicial es null (onGoing game)
 
-// ------------------ definir ganador -----------------
 
-const checkWinner = (board: (null | string)[]) => {
+
+// ------------------------- manejo de estados iniciales en React ---------------------
+
+// En Reac con TS, cuando asignas un estado inicial al useState, TS infiere que el estado va a ser del mismo type del estado inicial
+
+// Para arreglar esto, se necesita decirle explicitamente a useState que type va a ser el estado
+
+// En TypeScript, los hooks de React como useState aceptan lo que se conoce como un tipo genérico utilizando la sintaxis < >. Si no se lo pasas, TypeScript infiere el tipo basándose únicamente en el valor inicial
+
+
+// -------------------------------- FUNCIONES --------------------------------------
+
+
+// ------------------ funcion para definir ganador -----------------
+
+const checkWinner = (boardToCheck: (null | string)[]): WinnerState => {
+
+  // el parámetro board está definido como (null | string)[]. Al extraer una posición del tablero (const win = board[a]), TypeScript asume que win es un string general.
 
   for (const combo of WINNER_COMBOS) {
 
     const [a, b, c] = combo; // capturamos las posciones
     if (
-      board[a] && // las comparamos con el tablero
-      board[a] === board[b] && 
-      board[a] === board[c]) {
+      boardToCheck[a] && // las comparamos con el tablero
+      boardToCheck[a] === boardToCheck[b] && 
+      boardToCheck[a] === boardToCheck[c]) {
 
-      const win = board[a];
-      return win;
+      const win = boardToCheck[a];
+      return win as WinnerState;
+
+      // por eso hay que decirle explicitamente a la funcion que no nos va a retornar un string normal, nos va a retornar el type WinnerState
     }
-  }
-  
-  if (!board.includes(null)) {
-    return WINNER.tie}
+}
+  // si hay empate:
+  if (!boardToCheck.includes(null)) {return WINNER.isTie as WinnerState}
 
-  // Si no hay ganador ni empate, y el juego sigue en curso
-  return null; 
-  
+
+    // Si no hay ganador ni empate, y el juego sigue en curso:
+    return null
+
   }
+
+
+
+
 
 
 // ----------- función para cambiar el estado del turno -------------
 
-const updateBoard = (board: (null | string)[], index: number) => {
+const updateBoard = (boardToCheck: (null | string)[], index: number) => {
 
   // ------------- evitar que los valores se sobrescriban -------------
 
-  if (board[index]) { //si board en el indice indicado existe (no es null), cortar la función 
+  if (boardToCheck[index] || winner) { 
     return;
+    //si board en el indice indicado existe (no es null) o si hay un ganador (no es null), cortar la función y se para el juego
   }
 
   // ----------- actualizar el tablero ----------
 
-  const newBoard = [...board];
+  const newBoard = [...boardToCheck];
   // se hace una copia del board porque NUNCA hay que alterar las props 
   // siempre se tiene que crear un array NUEVO con las modificaciones 
   // las consecuencias de esto es que pueden haver problemas de renderizado 
@@ -142,40 +175,69 @@ const updateBoard = (board: (null | string)[], index: number) => {
   
 // ------------- actualizar el turno ----------------
 
-  const newTurn = turn === TURNS.x ? TURNS.o : TURNS.x;
+  const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
   setTurn(newTurn); // funcion para cambiar de un turno a otro
 
-  // ------------------ definir ganador -----------------
-
+  // ------------------ revisar si hay ganador -----------------
 
   const isWinner = checkWinner(newBoard);
 
+  // ------------------ asincronia en react -------------------
+
+  // si aqui le pasaramos el Board original con el estado, 
+  // como el estado del tablero es asincrono, nos deja hacer una jugada mas antes de parar
+  // porque todavia NO tenia la respuesta actual del estado del tablero sino el anterior estado, pero si siguio ejecutando el codigo 
+
+  // hay dos formas de solucionar esto: 
+  // pasar una copia del valor del estado o
+  // pasar una funcion callback que devuelva una copia del estado
+  // pero no podemos hacer ningun async await porque useState no devuelve una promesa 
+
+  // -----------------------------------------------------------
+
     // Si el juego sigue en curso (osea, isWinner no es null)
-   // Si isWinner tiene un valor (es "x", "o" o "nobody won"), actualizamos el estado
+   // Si isWinner tiene un valor (es "x", "o" o "nobody"), actualizamos el estado
 
-  if (isWinner) {
-    setWinner(isWinner);
+  if (isWinner !== null) {
+    setWinner(isWinner); 
+    // -----------------------------------------------------
+
+    // el setWinner actualiza el estado de forma ASINCRONA 
+
+  // y como anunciamos quien gano? 
+  // en teoria, si ponemos un alert despues del setWinner, se muestra 
+
+  // alert(`El ganador es ${isWinner}`)
+    // aparece el alert, pero visualmente, se coloca el ultimo movimiendo ganador DESPUES del alert
+
+    // esto quiere decir que la actualizacion de estados en react es ASINCRONO
+    // Osea, NO BLOQUEA EL CODIGO QUE VIENE DESPUES
+    // el estado se puede actualizar en 4, 7, 10 segundos, pero se ejecuta el codigo que viene despues 
+    // NO puedes contar con que vas a tener el valor actualizado de state 
+    // el codigo que sigue puede trabajar con el valor anterior del state
+
+    // por eso es que a checkWinner le pasamos una COPIA del board. Si le pasamos el board original (que contiene el estado del board), NO HAY GARANTIA DE QUE TENGA EL ESTADO porque la actualizacion del estado es ASINCRONA
+
   }
-  
 };
-
 
 // --------------------- función para resetear el juego --------------------
 
 function resetGame () {
   setBoard(Array(9).fill(null));
-  setTurn(TURNS.x);
-}
+  setTurn(TURNS.X);
+  setWinner(null);
+}; 
 
 // ----------------- función para renderizar los square por cada elemento de board ------------------
 
-function displayBoard(board: (null | string)[]) {
+function displayBoard(boardToCheck: (null | string)[]) {
 
-    return board.map((___, index) => {
+    return boardToCheck.map((___, index) => {
       return (
         <Square 
         key={index}
-        updateBoard={() => updateBoard(board, index)}
+        updateBoard={() => updateBoard(boardToCheck, index)}
         >
           {board[index]}
         </Square>
@@ -187,22 +249,55 @@ function displayBoard(board: (null | string)[]) {
 //De esta manera, <Square> recibe una función que se ejecutará solo en el futuro, cuando el usuario haga click, y que recordará los parámetros board e index.
 }; 
 
+
+
+
+
+
 // -------------------- App ------------------------
 
   return (
     <main className="board">
+      <h1>Tic Tac Toe</h1>
+
       <section className='game'>
         {displayBoard(board)}
       </section>
 
       <section className="turn">
-        <Square isSelected={turn === TURNS.x}>{TURNS.x}</Square>
-        <Square isSelected={turn === TURNS.o}>{TURNS.o}</Square>
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
 
       <div>
         <button onClick={resetGame}>Reset Game</button>
       </div>
+      
+    {
+      winner != null && (
+
+        <section className='winner'>
+          <div className='text'>
+            <h2>
+              {
+                winner === false 
+                ? "Empate"
+                : `El ganador es:`
+              }
+
+            </h2>
+            <header className='win'>
+              {winner && <Square>{winner}</Square>}
+            </header>
+
+            <footer>
+              <button onClick={resetGame}>Reset Game</button>
+            </footer>
+              
+          </div>
+        </section>
+      )
+    }
     </main>
   )
 
