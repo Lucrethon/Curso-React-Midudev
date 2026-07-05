57e3f122e0f3fc10c1ce67d9cc46697dab44f393
Lucrethon <lucrecimich@gmail.com>
2026-07-05 19:24:01 -0400

    feat: add CarruselEmoji component for infinite emoji carousel functionality

--- Projects/06_Exercises/src/App.tsx
+++ Projects/06_Exercises/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
 import './App.css'
 
 // Ejercicios para repasar metodos de Arrays con TS y React
@@ -156,6 +156,47 @@ const SelectorFrutas = () => {
       )
     }
   )
+};
+
+// Ejercicio 5: Carrusel Infinito (Lógica de índices y límites)
+// Este es un clásico del desarrollo web. No vas a iterar sobre todo el array a la vez, sino que vas a usar un estado numérico para "apuntar" a un solo elemento del array en un momento dado, y te moverás por él.
+
+// El Reto:
+// 1. Crea un componente llamado <CarruselEmojis/>.
+// 2. No uses un estado para la lista, solo una constante simple fuera o dentro del componente:
+// const emojis = ["🚀", "🎸", "👾", "🦖", "🍔"];
+
+// 3. Crea un estado para controlar dónde estás parado: const [indiceActual, setIndiceActual] = useState(0).
+// 4. En la pantalla debe renderizarse en gigante solo el emoji actual (usando emojis[indiceActual]).
+
+// 5. Crea dos botones: "Anterior" y "Siguiente".
+// Si estás en el 0 y le dan a "Anterior", debe saltar al último elemento (el 4).
+
+const CarruselEmoji = () => {
+
+  const [currentIndex, setCurrentIndex] = useState<number>(0); 
+  const emojis = ["🚀", "🎸", "👾", "🦖", "🍔"];
+  
+
+  const next = () => {
+    if (currentIndex === emojis.length-1) setCurrentIndex(0)
+    else setCurrentIndex(currentIndex + 1)
+  };
+
+  const previous = () => {
+    if (currentIndex === 0) setCurrentIndex(emojis.length - 1)
+      else setCurrentIndex(currentIndex - 1)
+  };
+
+  const currentEmoji = emojis[currentIndex];
+
+  return (
+    <div>
+      <button onClick={previous}>{"<"}</button>
+      {currentEmoji}
+      <button onClick={next}>{">"}</button>
+    </div>
+  )
 }
 
 
@@ -203,6 +244,10 @@ function App() {
       <div>
         <h1>Ejercicio de Frutas</h1>
         <SelectorFrutas />
+      </div>
+      <div>
+        <h1>Ejercicio de Carrusel de Emojis</h1>
+        <CarruselEmoji />
       </div>
     </div>
   </>
