64154b201e32ce87fad7b356363acac6a78be65d
Lucrethon <lucrecimich@gmail.com>
2026-07-06 10:11:26 -0400

    Renaming project

---
+++ Projects/07_Fetching_with_React/src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+// # Prueba técnica para Juniors y Trainees de React en Live Coding
+
+// APIs:
+
+// - Facts Random: https://catfact.ninja/fact    MD034/no-bare-urls: Bare URL used
+// - Imagen random: https://cataas.com/cat/says/hello    MD034/no-bare-urls: Bare URL used
+
+// Recupera un hecho aleatorio de gatos y muestra una imagen de un gato con la primera palabra del hecho de la segunda API 
+
+function App() {
+
+  return (
+    <>
+  
+    </>
+  )
+}
+
+export default App
