df845bf70fc41b9979d31b325d92bc0734c15973
Lucrethon <lucrecimich@gmail.com>
2026-07-06 16:24:14 -0400

    create component for fetching and displaying characters from API

--- Projects/07_Fetching_with_React/src/App.tsx
+++ Projects/07_Fetching_with_React/src/App.tsx
@@ -1,20 +1,138 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
-// # Prueba técnica para Juniors y Trainees de React en Live Coding
+// Pricipio de un fetching de datos: 
 
-// APIs:
+// Se manejan tres estados: loading, data y error.
+// 1. data: es la data que se obtiene de la API
+// 2. loading: es un booleano que indica si la data se está cargando. Se utiliza para mostrar un spinner o un mensaje de carga mientras se espera la respuesta de la API
+// 3. error: es un booleano que indica si hubo un error al obtener la data. Se utiliza para mostrar un mensaje de error al usuario en caso de que la API no responda o haya algún problema con la conexión a internet.
 
-// - Facts Random: https://catfact.ninja/fact    MD034/no-bare-urls: Bare URL used
-// - Imagen random: https://cataas.com/cat/says/hello    MD034/no-bare-urls: Bare URL used
+// el lugar correcto donde se hace el fetching de datos es en un useEffect con un array de dependencias vacío, para que se ejecute una sola vez al montar el componente.
 
-// Recupera un hecho aleatorio de gatos y muestra una imagen de un gato con la primera palabra del hecho de la segunda API 
+// Ejercicio: Crea un componente llamado <DirectorioPersonajes/>.
+
+// Debes hacer un fetch a esta URL exacta: 
+// [https://rickandmortyapi.com/api/character]
+// (https://rickandmortyapi.com/api/character)
+
+// Implementa los tres estados: personajes, cargando, y error.
+
+// Muestra un texto de "Cargando personajes..." mientras la petición se resuelve.
+
+// Una vez que lleguen los datos, usa un .map() para renderizar una lista o grilla.
+
+// Por cada personaje, debes mostrar al menos:
+
+// Su nombre.
+// Su especie (species).
+// Su foto (la API te devuelve una URL directa a la imagen en la propiedad image, puedes usarla en una etiqueta <img src={...} />).
+
+// Cuando hagas await respuesta.json(), el servidor no te va a devolver el array directamente. Te va a devolver un objeto gigante con información sobre la paginación. El array de personajes vive dentro de una propiedad llamada results
+
+const URL = "https://rickandmortyapi.com/api/character";
+
+// de quiktype.io: 
+
+
+type Location = {
+    name: string;
+    url:  string;
+}
+
+type Status = "Alive" | "Dead" | "unknown";
+type Gender = "Female" | "Male" | "unknown";
+type Species = "Alien" | "Human";
+
+type Personaje = {
+    id:       number;
+    name:     string;
+    status:   Status;
+    species:  Species;
+    type:     string;
+    gender:   Gender;
+    origin:   Location;
+    location: Location;
+    image:    string;
+    episode:  string[];
+    url:      string;
+    created:  Date;
+}
+
+const DirectorioPersonajes = () => {
+
+  const [personajes, setPersonajes] = useState<Personaje[]>([]);
+  const [error, setError] = useState<string | null>(null); 
+  const [loading, setLoading] = useState(true);
+
+  useEffect(
+    ()=>{
+      const getData = async () => {
+        try {
+          const respuesta = await fetch(URL); 
+          // Verificar respuesta (si hay error)
+          if (!respuesta.ok) throw new Error("Error en la conexión");
+          // Convertir en JSON
+          const personajes = await respuesta.json();
+          // Guardar los datos en el estado 
+          setPersonajes(personajes.results)
+
+        } catch (err) { 
+          //'err' es unknown por defecto
+          // por ende, se tiene que comprobar el tipo de err con instanceof antes de acceder a sus propiedades 
+
+            // 1. Verificamos específicamente si es un TypeError
+            if (err instanceof TypeError) {
+              setError(err.message)
+            }
+            // 2. Verificamos si es un Error general (ReferenceError, SyntaxError, etc.)
+            else if (err instanceof Error) {
+              setError(err.message)
+            }
+            // 3. Fallback: capturamos strings, números o cosas extrañas arrojadas (throw "error")
+            else {setError(String(err))}
+        } finally {
+          // pase lo que pase, apagamos el estado de "cargando"
+          setLoading(false)
+        }
+      };
+      getData(); 
+    }, []);
+
+    const displayPersonajes = (arrayPersonajes: Personaje[]) => {
+        return arrayPersonajes.map(
+          (personaje: Personaje) => {
+            return (
+              <div key={personaje.id}>
+                <strong>{personaje.name}</strong>
+                <p>{`Raza: ${personaje.species}`}</p>
+                <div>
+                  <img src={personaje.image} alt={`Foto de ${personaje.name}`} />
+                </div>
+              </div>
+            )
+          }
+        )
+    }
+
+    if (loading) return <p>Cargando personajes...</p>
+    if (error) return <p>Hubo un error: {error}</p>
+    // si estos dos if pasan, es que los datos cargaroncorrectamernte 
+
+    return (
+      <div>
+        {displayPersonajes(personajes)}
+      </div>
+    )
+
+}
+
 
 function App() {
 
   return (
     <>
-  
+      <DirectorioPersonajes />
     </>
   )
 }
