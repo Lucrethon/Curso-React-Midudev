9ec734efacbc3079c56394fe7b4d4031adff4558
Lucrethon <lucrecimich@gmail.com>
2026-07-08 16:08:33 -0400

    feat: add custom hooks for fetching cat facts and images

---
+++ Projects/08_Junior_Thecnical_Test/src/Hooks/useCatFact.tsx
@@ -0,0 +1,56 @@
+import {useEffect, useState} from 'react'
+const catEndpointRandomFact = "https://catfact.ninja/fact "
+
+export const useCatfact = () => {
+    const [fact, setFact] = useState<string>("")
+    const [factError, setFactError] = useState<string | null>(null)
+    const [factLoading, setFactLoading] = useState<boolean>(true)
+
+// obtener la el dato curioso cada vez que se renderice la pagina
+
+    const getFact = async () => {
+    try {
+        // con async - await
+        const res = await fetch(catEndpointRandomFact);
+        const data = await res.json();
+        const fact = data.fact as string
+        setFact(fact)
+        
+        // SIN async - await seria asi: 
+
+        // fetch(catEndpointRandomFact)
+        // .then(res => res.json())
+        // .then(data => setFact(data.fact))
+
+    } catch(err) {
+        
+        if (err instanceof Error) {
+            setFactError(err.message)
+        }
+        else if (err instanceof TypeError) {
+            setFactError(err.message)
+        }
+        else {
+            (setFactError(`${String(err)}`))
+        }
+    } finally {
+        setFactLoading(false)
+    };
+    };
+
+    useEffect(
+        ()=> {getFact()}, []
+    );
+    // Devolvemos un objeto con los TRES estados ya procesados
+    return { fact, factError, factLoading, getFact };
+
+}
+
+
+    // las dependencias vacias hacen que se ejecute una sola vez al montar el componente
+
+    // si el efecto es un condicional del useEffect, ahi se tiene que colocar fact en las dependencias
+    // si se pone fact, se ejecutaria cada vez que fact cambie (osea, cada vez que se haga un setFact), y eso generaria un loop infinito
+
+    // si no se coloca ningun array, se genera un loop infinito, porque cada vez que se ejecuta el useEffect, se cambia el estado de fact, y eso hace que se vuelva a ejecutar el useEffect
+    // esto puede servir para tracking cambios de estado, pero en este caso no es necesario
---
+++ Projects/08_Junior_Thecnical_Test/src/Hooks/useCatImageUrl.tsx
@@ -0,0 +1,40 @@
+import {useEffect, useState} from 'react'
+
+// const catEndpointImageUrl = `https://cataas.com/cat/says/${firstWord}?json=true`
+
+export const useCatImageUrl = (fact: string) => {
+
+    const [imageUrl, setImageUrl] = useState<string>("")
+    const [imageUrlError, setImageUrlError] = useState<string | null>(null)
+    const [imageUrlLoading, setImageUrlLoading] = useState<boolean>(true)
+
+
+    // obtener la imagen cada vez que cambie el hecho
+    // con solamente fetch 
+
+    const getImageUrl = () => {
+        // recuperar la primera palabra del fact 
+        const firstWord = fact.split(" ")[0]
+        // recuperar las tres primeras palabras del fact
+        // const threeWords = fact.split(" ").slice(0, 3).join(" ")
+
+        return fetch(`https://cataas.com/cat/says/${firstWord}?json=true`)
+        .then(res => {
+            if (!res.ok) throw new Error("No se ha podido cargar la imagen")
+            return res.json()
+        })
+        .then(picture => setImageUrl(picture.url)
+        )
+        .catch((err) => setImageUrlError(err))
+        .finally(() => setImageUrlLoading(false))
+    }
+
+    useEffect(
+        () => {
+            if (!fact) return // si no hay un hecho, no se ejecuta el efecto
+            getImageUrl()
+        },[fact])
+        // NO se puede entregar la imagen hasta no tener el hecho, por eso se coloca como dependencia el fact 
+
+    return { imageUrl, imageUrlError, imageUrlLoading }
+}
