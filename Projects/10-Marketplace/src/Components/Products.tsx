061d0093c1c2f440e321bf5f5140a71c720699e2
Lucrethon <lucrecimich@gmail.com>
2026-07-30 16:10:50 -0400

    feat: style Products component and update HTML structure for better semantics

--- Projects/10-Marketplace/src/Components/Products.tsx
+++ Projects/10-Marketplace/src/Components/Products.tsx
@@ -5,17 +5,17 @@ import "./Product.css"
 export const Products = ({productList} : {productList: Product[]}) => {
 
     return(
-        <main className="product-list">
+        <main className="products">
         <ul>
             {productList.map((product) =>{
                 return (
                     <li key={product.id} className="product">
                         <img src={product.thumbnail} alt={`Image of ${product.title}`}></img>
                         <div>
-                            <strong className="product-title">{product.title}</strong>
-                            <p className="product-description">{product.description}</p>
-                            <p className="product-stock">{`Stock: ${product.stock}`}</p>
-                            <p className="product-price">{`Price: ${product.price}`}</p>
+                            <h3 className="product-title">{product.title}</h3>
+                            <span className="product-description">{product.description}</span>
+                            <span className="product-stock">{`Stock: ${product.stock}`}</span>
+                            <span className="product-price">{`Price: ${product.price}`}</span>
                         </div>
                         <div>
                             <button>
