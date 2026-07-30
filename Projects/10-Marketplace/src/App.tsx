e07383b6495b08e4bac79516611cdeb105699f96
Lucrethon <lucrecimich@gmail.com>
2026-07-30 18:38:09 -0400

    feat: enhance App component structure and integrate product loading state

--- Projects/10-Marketplace/src/App.tsx
+++ Projects/10-Marketplace/src/App.tsx
@@ -1,10 +1,25 @@
+import { Products } from "./Components/Products"
+import { useProducts } from "./Hooks/useProducts"
+import type { ProductList } from "./types"
+import mockProducts from "./Mocks/products.json"
+
 
 
 const App = () => {
+    
+    const productList = mockProducts as ProductList
+    const {products, error, loading} = useProducts()
 
     return (
         <>
-        
+            <h1>Carrito de Compras</h1>
+            <div>
+                {
+                    loading
+                    ? <span>Cargando...</span>
+                    : <Products productList={products} error={error}/>
+                }
+            </div>
         </>
     )
 }
