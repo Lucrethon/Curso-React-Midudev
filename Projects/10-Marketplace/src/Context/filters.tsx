63c7b7a52a0f759db73c4a394cfb61bc5cdf223f
Lucrethon <lucrecimich@gmail.com>
2026-07-31 18:21:37 -0400

    feat: integrate useFilter and useProducts in FilterProvider for enhanced filtering logic

--- Projects/10-Marketplace/src/Context/filters.tsx
+++ Projects/10-Marketplace/src/Context/filters.tsx
@@ -1,28 +1,38 @@
 import { createContext, useContext, type ReactNode } from "react";
-import type { Filter } from "../types";
+import type { Filter, Product } from "../types";
+import { useFilter } from "../Hooks/useFilter";
+import { useProducts } from "../Hooks/useProducts";
 
 // createContext, useContext: te permite pasar props (valores, funciones o estados) de un componente padre a uno hijo
 // Todos los componentes que esten dentro de <Context.Provider value={prop}> van a poder utilizar el value (un estado, función, setter, etc)
 
+
 type FilterContextType = {
     setFilters : React.Dispatch<React.SetStateAction<Filter>>,
     filters : Filter
+    filteredProducts : Product[]
 }
 
 // Context: 
 const FilterContext = createContext<FilterContextType | null>(null);
 
 
-type FilterProviderProps = {
-    children : ReactNode,
-    setFilters : React.Dispatch<React.SetStateAction<Filter>>
-    filters : Filter
-}
+// type FilterProviderProps = {
+//     children : ReactNode,
+//     setFilters : React.Dispatch<React.SetStateAction<Filter>>
+//     filters : Filter
+// }
+
 // para simplificar, aquí se crea un componente en donde ya esta el Context.Provider
 // se le pasa el children (componente hijo que va a utilizar la prop) y la prop necesaria 
-export const FilterProvider = ({children, setFilters, filters} : FilterProviderProps) => {
+// un provider al final es un componente 
+export const FilterProvider = ({children} : {children: ReactNode}) => {
+    
+    const {products} = useProducts()
+    const { filteredProducts, setFilters, filters} = useFilter({products})
+
     return (
-        <FilterContext.Provider value={{setFilters, filters}}>
+        <FilterContext.Provider value={{ filteredProducts, setFilters, filters}}>
             {children}
         </FilterContext.Provider>
     )
--- Projects/10-Marketplace/src/main.tsx
+++ Projects/10-Marketplace/src/main.tsx
@@ -1,10 +1,13 @@
 import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { FilterProvider } from './Context/filters'
 import App from './App'
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
-    <App/>
+    <FilterProvider>
+      <App/>
+    </FilterProvider>
   </StrictMode>
 )
