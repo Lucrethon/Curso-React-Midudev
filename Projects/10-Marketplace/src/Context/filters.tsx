1487b602592c9397047f1994661149b02b2d75fd
Lucrethon <lucrecimich@gmail.com>
2026-07-31 15:01:08 -0400

    comments

--- Projects/10-Marketplace/src/Context/filters.tsx
+++ Projects/10-Marketplace/src/Context/filters.tsx
@@ -1,10 +1,14 @@
 import { createContext, useContext, type ReactNode } from "react";
 import type { Filter } from "../types";
 
+// createContext, useContext: te permite pasar props (valores, funciones o estados) de un componente padre a uno hijo
+// Todos los componentes que esten dentro de <Context.Provider value={prop}> van a poder utilizar el value (un estado, función, setter, etc)
+
 type FilterContextType = {
     setFilters : React.Dispatch<React.SetStateAction<Filter>>
 }
 
+// Context: 
 const FilterContext = createContext<FilterContextType | null>(null);
 
 
@@ -12,14 +16,22 @@ type FilterProviderProps = {
     children : ReactNode,
     setFilters : React.Dispatch<React.SetStateAction<Filter>>
 }
-
+// para simplificar, aquí se crea un componente en donde ya esta el Context.Provider
+// se le pasa el children (componente hijo que va a utilizar la prop) y la prop necesaria 
 export const FilterProvider = ({children, setFilters} : FilterProviderProps) => {
     return (
         <FilterContext.Provider value={{setFilters}}>
             {children}
         </FilterContext.Provider>
     )
-}   
+}    
+
+// para simplificar, se crea tambien un hook donde ya se use el useContext
+// se simplifica la logica de: 
+// 1. pasarle el context al useContext (const context = useContext(ContextName));
+// 2. Si usas TS e inicializas el default value con null, se puede decir que es null. Dentro del hook haces: 
+// if (!context) return null; const { props } = context  
+// return context 
 
 export const useFilter = () => {
     const context = useContext(FilterContext)
