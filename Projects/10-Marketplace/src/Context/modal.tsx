b10f88bac10f3a0af96af86ffda4a26999332681
Lucrethon <lucrecimich@gmail.com>
2026-08-04 17:02:50 -0400

    feat: add ModalContext for managing modal state in the application

---
+++ Projects/10-Marketplace/src/Context/modal.tsx
@@ -0,0 +1,29 @@
+import { createContext, type ReactNode } from "react";
+import { useContext, useState } from "react";
+
+type ModalContextType = {
+    isModalOpen : boolean,
+    setIsModalOpen : React.Dispatch<React.SetStateAction<boolean>>,
+}
+
+const ModalContext = createContext<ModalContextType | null>(null)
+
+export const ModalProvider = ({children} : {children: ReactNode}) => {
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false) 
+
+    return (
+        <ModalContext.Provider value={{isModalOpen, setIsModalOpen}}>
+            {children}
+        </ModalContext.Provider>
+    )
+}   
+
+export const useModalContext = () => {
+    const context = useContext(ModalContext)
+
+    if (!context) {
+        throw new Error('useFilter se debe usar dentro de un ModalContextProvider')
+    }
+
+    return context
+}
