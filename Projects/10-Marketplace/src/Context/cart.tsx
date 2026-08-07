1ea74ff70bd92a7a0785241b54e2ce532fc17592
Lucrethon <lucrecimich@gmail.com>
2026-08-06 21:03:27 -0400

    feat: implement useReducer for cart management and add cart actions

--- Projects/10-Marketplace/src/Context/cart.tsx
+++ Projects/10-Marketplace/src/Context/cart.tsx
@@ -1,6 +1,7 @@
 import React, { createContext, type ReactNode } from "react";
 import { useState, useContext } from "react";
 import type { Product, CartItem } from "../types";
+import { useReducer } from "react";
 
 type CartContextType = {
     cartList: CartItem[], 
@@ -12,11 +13,94 @@ type CartContextType = {
     clearAllItems : (product : CartItem | Product) => void
 }
 
+type AddToCart = {
+    type: 'ADD_TO_CART'
+    payload: Product
+} 
+
+type RemoveFromCart = {
+    type: 'REMOVE_FROM_CART'
+    payload: CartItem
+}
+
+type ClearCart = {
+    type: 'CLEAR_CART'
+}
+
+type ClearAllItems = {
+    type: 'CLEAR_ALL_ITEMS'
+    payload: Product | CartItem
+}
+
+type CartAction = 
+    |AddToCart
+    |RemoveFromCart
+    |ClearCart
+    |ClearAllItems;
+
+
+
 const CartContext = createContext<CartContextType | null>(null)
 
 export const CartProvider = ({children} : {children: ReactNode}) => {
 
     const [cartList, setCartList] = useState<CartItem[]>([])
+
+    
+
+    const initialState = [] as CartItem[]
+    const cartReducer = ({state, action}: {state: CartItem[], action: CartAction}) => {
+
+
+        const isProductOnCart = (product: Product) => {
+            const productInCart = state.find((item) => item.id === product.id) // falsy or truty
+            return !!productInCart
+        }
+
+        switch (action.type) {
+
+            case 'CLEAR_CART': {
+                return initialState
+            }
+
+            case 'CLEAR_ALL_ITEMS': {
+                const product = action.payload
+                return state.filter(item => item.id !== product.id)
+            }
+
+            case 'ADD_TO_CART': {
+                const product = action.payload
+                if (isProductOnCart(product)) { // si el rpoducto ya existe en el carrito, solo se actualiza la cantidad 
+                    return state.map(
+                        (item) => item.id === product.id
+                        ? {... item, quantity: item.quantity + 1}
+                        : item
+                    )
+                }
+                else return [
+                    ...state,       // 1. Desempaqueta todos los elementos que YA estaban en el carrito
+                    {               // 2. Agrega este NUEVO objeto (product) al final del arreglo
+                        ...product, 
+                        quantity: 1 // 3. añadiendole la propiedad quantity
+                    } as CartItem
+                ]
+            }
+
+            case 'REMOVE_FROM_CART': {
+                const product = action.payload
+                 if (product.quantity > 1) {
+                    return state.map(
+                        (item) => item.id === product.id
+                        ? {... item, quantity: item.quantity - 1}
+                        : item
+                )
+                }
+                else state.filter(item => item.id !== product.id)
+            }
+
+        }
+        
+    }
 
     const addToCart = (product: Product) => {
 
