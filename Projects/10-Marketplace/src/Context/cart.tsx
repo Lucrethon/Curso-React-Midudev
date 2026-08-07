fc717f410d0e9d64d9fffc98220042f1c88795fc
Lucrethon <lucrecimich@gmail.com>
2026-08-06 21:12:26 -0400

    feat: enhance cartReducer to manage product quantities and clear cart functionality

--- Projects/10-Marketplace/src/Context/cart.tsx
+++ Projects/10-Marketplace/src/Context/cart.tsx
@@ -39,68 +39,69 @@ type CartAction =
     |ClearAllItems;
 
 
+const initialState = [] as CartItem[]
+const cartReducer = (state: CartItem[], action: CartAction) => {
+
+
+    const isProductOnCart = (product: Product) => {
+        const productInCart = state.find((item) => item.id === product.id) // falsy or truty
+        return !!productInCart
+    }
+
+    switch (action.type) {
+
+        case 'CLEAR_CART': {
+            return initialState
+        }
+
+        case 'CLEAR_ALL_ITEMS': {
+            const product = action.payload
+            return state.filter(item => item.id !== product.id)
+        }
+
+        case 'ADD_TO_CART': {
+            const product = action.payload
+            if (isProductOnCart(product)) { // si el rpoducto ya existe en el carrito, solo se actualiza la cantidad 
+                return state.map(
+                    (item) => item.id === product.id
+                    ? {... item, quantity: item.quantity + 1}
+                    : item
+                )
+            }
+            else return [
+                ...state,       // 1. Desempaqueta todos los elementos que YA estaban en el carrito
+                {               // 2. Agrega este NUEVO objeto (product) al final del arreglo
+                    ...product, 
+                    quantity: 1 // 3. añadiendole la propiedad quantity
+                } as CartItem
+            ]
+        }
+
+        case 'REMOVE_FROM_CART': {
+            const product = action.payload
+                if (product.quantity > 1) {
+                return state.map(
+                    (item) => item.id === product.id
+                    ? {... item, quantity: item.quantity - 1}
+                    : item
+            )
+            }
+            else return state.filter(item => item.id !== product.id)
+        }
+
+    default: return state
+
+    }
+    
+}
 
 const CartContext = createContext<CartContextType | null>(null)
 
 export const CartProvider = ({children} : {children: ReactNode}) => {
 
+    const [state, dispatch] = useReducer(cartReducer, initialState)
+
     const [cartList, setCartList] = useState<CartItem[]>([])
-
-    
-
-    const initialState = [] as CartItem[]
-    const cartReducer = ({state, action}: {state: CartItem[], action: CartAction}) => {
-
-
-        const isProductOnCart = (product: Product) => {
-            const productInCart = state.find((item) => item.id === product.id) // falsy or truty
-            return !!productInCart
-        }
-
-        switch (action.type) {
-
-            case 'CLEAR_CART': {
-                return initialState
-            }
-
-            case 'CLEAR_ALL_ITEMS': {
-                const product = action.payload
-                return state.filter(item => item.id !== product.id)
-            }
-
-            case 'ADD_TO_CART': {
-                const product = action.payload
-                if (isProductOnCart(product)) { // si el rpoducto ya existe en el carrito, solo se actualiza la cantidad 
-                    return state.map(
-                        (item) => item.id === product.id
-                        ? {... item, quantity: item.quantity + 1}
-                        : item
-                    )
-                }
-                else return [
-                    ...state,       // 1. Desempaqueta todos los elementos que YA estaban en el carrito
-                    {               // 2. Agrega este NUEVO objeto (product) al final del arreglo
-                        ...product, 
-                        quantity: 1 // 3. añadiendole la propiedad quantity
-                    } as CartItem
-                ]
-            }
-
-            case 'REMOVE_FROM_CART': {
-                const product = action.payload
-                 if (product.quantity > 1) {
-                    return state.map(
-                        (item) => item.id === product.id
-                        ? {... item, quantity: item.quantity - 1}
-                        : item
-                )
-                }
-                else state.filter(item => item.id !== product.id)
-            }
-
-        }
-        
-    }
 
     const addToCart = (product: Product) => {
 
