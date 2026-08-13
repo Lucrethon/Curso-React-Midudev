a8282ea4d1bc859e390be7e945d4245a498f4428
Lucrethon <lucrecimich@gmail.com>
2026-08-13 18:47:07 -0400

    feat: enhance Link component to support accessibility and SPA navigation

--- Projects/11-React_Router/src/components/Link.tsx
+++ Projects/11-React_Router/src/components/Link.tsx
@@ -2,8 +2,9 @@ import type React from "react"
 import { EVENTS, TARGET } from "../types"
 // import type { Urls } from "../types"
 
-// Que debemos hacer para hacer una single page aplication? 
-export const navigate = (href: string) => {
+// Que debemos hacer para hacer una single page aplication (SPA)? 
+export const navigate = (href: string) => { // funcion para navegar con SPA
+
     // el objeto history de window sirve para interactuar y manipular el historial de navegación de la pestaña actual del navegador.
     // pero para cambiarla y no hacer una recarga completa de la página utilizamos el método pushState
     // pushState Añade una nueva entrada al historial de navegación y cambia la URL en la barra de direcciones sin recargar la página. Ejemplo: 
@@ -29,9 +30,27 @@ export const Link = ({ target = TARGET.SELF, to, children, ...props} : {target?:
 
 
     const handleClick = (event: React.MouseEvent) => {
-        event.preventDefault()
-        // es importante el prevent default porque si no se coloca, se renderiza toda la pagina otra vez como si fuera un multiple page aplication 
-       navigate(to) 
+
+        // para que funcionen las opciones de accecibilidad se realizan las siguientes comprobaciones: 
+
+        // si es el evento o click principal 
+        const isMainEvent = event.button === 0 // primary click
+
+        // si el evento esta modificado (es decir, si no se esta abriendo el link con click sino con un comando por ejemplo. Ej: mayuscula + click abre en otra ventana)
+        // esto es para que el usuario pueda realizar esas acciones en nuestra pagina 
+        const isModifiedEvent = event.metaKey || event.altKey || event.ctrlKey || event.shiftKey
+
+        // si se está abriendo en la misma pestaña (comportamiento por defecto)
+        const isManageableEvent = target === undefined || target === TARGET.SELF
+
+        if (isMainEvent && !isModifiedEvent && isManageableEvent) {
+            // si estas condiciones se cumplen, se hace la navegación son single page aplication 
+
+            event.preventDefault()
+            // es importante el prevent default porque si no se coloca, se renderiza toda la pagina otra vez como si fuera un multiple page aplication 
+            navigate(to) // navegacion con single page aplication 
+        } 
+       
     }
     return <a onClick={handleClick} href={to} target={target} {...props}>{children}</a>
 
