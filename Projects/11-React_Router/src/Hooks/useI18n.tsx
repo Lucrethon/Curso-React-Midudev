cd8b5810b9d2cc2b9989200466dfb402bba3818a
Lucrethon <lucrecimich@gmail.com>
2026-08-19 19:46:26 -0400

    feat(i18n): implement useI18n hook for language management and local storage integration

---
+++ Projects/11-React_Router/src/Hooks/useI18n.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react"
+import type { Lang, Pages } from "../types"
+import { Langs } from "../types"
+import { i18n } from "../i18n"
+
+
+export const useI18n = (lang = Langs.spanish) => {
+
+    const localStorage = window.localStorage.getItem('lang')
+    const initialState = localStorage ? JSON.parse(localStorage) : Langs.spanish
+    const [language, setLanguage] = useState<Lang>(initialState)
+
+    useEffect(() => {
+        window.localStorage.setItem('lang', JSON.stringify(language))
+    }, [language])
+
+    const changeLang = (lang: Lang) => {
+        setLanguage(lang)
+    }
+
+    const i18nObject = i18n[lang] as Pages
+
+    
+    return { language, changeLang, i18nObject }
+
+}
