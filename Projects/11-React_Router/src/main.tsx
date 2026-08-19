8e2217c2c17d4f17a3a6afd6369b0e107380689b
Lucrethon <lucrecimich@gmail.com>
2026-08-19 19:48:28 -0400

    feat(i18n): wrap App component with I18nProvider for internationalization support

--- Projects/11-React_Router/src/main.tsx
+++ Projects/11-React_Router/src/main.tsx
@@ -2,9 +2,13 @@ import './style.css'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import { StrictMode } from 'react'
+import { I18nProvider } from './Context/i18n'
 
 createRoot(document.getElementById('app') as HTMLElement).render(
+
   <StrictMode>
+    <I18nProvider>
     <App/>
+    </I18nProvider>
   </StrictMode>
 )
