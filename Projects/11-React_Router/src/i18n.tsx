667772bfae852ae71230c91adf115c06043ae9f4
Lucrethon <lucrecimich@gmail.com>
2026-08-19 14:40:16 -0400

    feat(i18n): restructure i18n object to include navigation options for AboutPage and HomePage

--- Projects/11-React_Router/src/i18n.tsx
+++ Projects/11-React_Router/src/i18n.tsx
@@ -1,13 +1,39 @@
  export const i18n = {
-    es : {
-        title: 'Sobre Mi',
-        description: 'Soy Super Lulu y estoy creando un clon de React Router!',
-        button: 'Volver al Home'
-    }, 
-    en : {
-        title: 'About Me',
-        description: "I'm Super Lulu and I'm creating a React Router clon!", 
-        button: 'Return to Home'
+    AboutePage : {
+        es : {
+            title: 'Sobre Mi',
+            description: 'Soy Super Lulu y estoy creando un clon de React Router!',
+            button: 'Volver al Home',
+            navigate: {
+                selecLanguage : 'Language:',
+                languageOpcions : {
+                    spanish: 'Spanish',
+                    english: 'English'
+                }
+            }
+        }, 
+        en : {
+            title: 'About Me',
+            description: "I'm Super Lulu and I'm creating a React Router clon!", 
+            button: 'Return to Home',
+            navigate: {
+                selecLanguage : 'Idioma:',
+                languageOpcions : {
+                    spanish: 'Español',
+                    english: 'Ingles'
+                }
+            }
+        }
+    },
+    HomePage : {
+        es : {
+            description: 'Pagina de ejemplo para crear un React Router desde cero hecha por Super Lulu',
+            navigate: 'Sobre Mi'
+        },
+        en : {
+            description: 'Example page to create a React Router from scratch by Super Lulu',
+            navigate: 'About Me'
     }
- }
+    }
 
+ }
