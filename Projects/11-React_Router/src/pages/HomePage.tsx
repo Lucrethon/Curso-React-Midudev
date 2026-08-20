fd472aeb8bac4180f8c6839ca6259e572d7fc435
Lucrethon <lucrecimich@gmail.com>
2026-08-20 19:53:23 -0400

    refactor(HomePage): integrate i18n for dynamic content rendering

--- Projects/11-React_Router/src/pages/HomePage.tsx
+++ Projects/11-React_Router/src/pages/HomePage.tsx
@@ -1,12 +1,20 @@
 import { URLs } from "../types"
 import { Link } from "../components/Link"
+import { useI18n } from "../Hooks/useI18n"
+import type { Params } from "../types"
 
-export const HomePage = () => {
+
+export const HomePage = ({routeParameters} : {routeParameters?: Params}) => {
+
+    const i18n = useI18n(routeParameters?.lang)
+    const { i18nObject } = i18n
+    const { homePage } = i18nObject
+
     return (
         <>
             <h1>Home</h1>
-            <p>Pagina de ejemplo para crear un React Router desde cero hecha por Super Lulu</p>
-            <Link to={URLs.ABOUT}>Sobre Mi</Link>
+            <p>{homePage.description}</p>
+            <Link to={URLs.ABOUT}>{homePage.navigate}</Link>
         </>
     )
 }
