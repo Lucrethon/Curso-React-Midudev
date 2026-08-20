0469bd7a34960c829d39c677a6c8dd93c185b026
Lucrethon <lucrecimich@gmail.com>
2026-08-20 19:53:41 -0400

    refactor(Language): integrate i18nObject for dynamic language options

--- Projects/11-React_Router/src/components/Language.tsx
+++ Projects/11-React_Router/src/components/Language.tsx
@@ -5,7 +5,7 @@ import { useI18nContext } from "../Context/i18n"
 export const Language = () => {
 
     const languageFilterId = useId()
-    const { changeLang } = useI18nContext()
+    const { changeLang, i18nObject } = useI18nContext()
 
     const handleLanguage = (event: React.ChangeEvent<HTMLSelectElement>) => {
     event.preventDefault();
@@ -15,12 +15,14 @@ export const Language = () => {
 
 }
 
+const { languageOptions } = i18nObject
+
     return (
     <form>
-        <label htmlFor={languageFilterId}>Idioma:</label>
+        <label htmlFor={languageFilterId}>{languageOptions.selectLanguage}</label>
         <select id={languageFilterId} name='language' onChange={handleLanguage}>
-            <option value={Langs.english}>English</option>
-            <option value={Langs.spanish}>Español</option>
+            <option value={Langs.english}>{languageOptions.languages.english}</option>
+            <option value={Langs.spanish}>{languageOptions.languages.spanish}</option>
         </select>
     </form>
     )
